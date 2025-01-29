e8c8f79154067b6feeeaff2242861c01fb6a2853
Yash <yashovardhanbiswas@gmail.com>
2025-01-29 16:13:35 -0500

    added Constants class and StringModifiers in Constants

---
+++ Constants.java
@@ -0,0 +1,15 @@
+public class Constants {
+    public static final class StringModifiers {
+    //Colors
+    public static final String redText = "\u001B[31m";
+    public static final String greenText = "\u001B[32m";
+    public static final String yellowText = "\u001B[33m";
+    public static final String magentaText = "\u001B[35m";
+    //Other Modifiers
+    public static final String italicsStart = "\033[3m";
+    public static final String italicsEnd = "\033[0m";
+    //Reset
+    public static final String reset = "\u001B[0m";
+
+    }
+}
