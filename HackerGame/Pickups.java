e83d37b5ec9c2b42e1899c96d2dbde0bba10ad35
Sanjeth Gobinath <sanjethgobinath@gmail.com>
2025-02-06 20:15:36 -0500

    complete file structure, bug fix needed

---
+++ HackerGame/Pickups.java
@@ -0,0 +1,64 @@
+package HackerGame;
+
+public class Pickups {
+    
+    public static void pickupScrewdriver(){
+        StdOut.println("You notice a screwdriver on the floor. Pick it up? [Y/N]");
+        while(true){
+            String choice = StdIn.readString().trim().toUpperCase();
+            if(choice.equals("Y")){
+                StdOut.println(Char.sMods.greenText + "Screwdriver " + Char.sMods.reset + "Obtained.");
+                Player.addItem("Screwdriver");
+                break;
+            }else if(choice.equals("N")){
+                StdOut.println("You ignored the screwdriver.");
+                break;
+            }else{
+                StdOut.println("invalid input.");
+            }
+        }
+
+        StdOut.println("Do you want to view your inventory? [Y/N]");
+        while(true){
+            String choice = StdIn.readString().trim().toUpperCase();
+            if(choice.equals("Y")){
+                Player.viewInventory();
+                break;
+            }else if(choice.equals("N")){
+                break;
+            }else{
+                StdOut.println("invalid input.");
+            }
+        }
+    }
+
+    public static void pickupHealth(){
+        StdOut.println("You notice a health pack on the floor. Pick it up? [Y/N]");
+        while(true){
+            String choice = StdIn.readString().trim().toUpperCase();
+            if(choice.equals("Y")){
+                StdOut.println(Char.sMods.greenText + "Health Pack " + Char.sMods.reset + "Obtained.");
+                Player.addItem("Health Pack");
+                break;
+            }else if(choice.equals("N")){
+                StdOut.println("You ignored the health pack.");
+                break;
+            }else{
+                StdOut.println("invalid input.");
+            }
+        }
+
+        StdOut.println("Do you want to view your inventory? [Y/N]");
+        while(true){
+            String choice = StdIn.readString().trim().toUpperCase();
+            if(choice.equals("Y")){
+                Player.viewInventory();
+                break;
+            }else if(choice.equals("N")){
+                break;
+            }else{
+                StdOut.println("invalid input.");
+            }
+        }
+    }
+}
---
+++ HackerGame/SoundPlayer.java
@@ -0,0 +1,59 @@
+package HackerGame;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class SoundPlayer {
+    private static Clip clip;
+    public static void PlayMusic(String location) {
+        new Thread(() -> {
+            try {
+                File musicPath = new File(location);
+                if (musicPath.exists()) {
+                    AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                    clip = AudioSystem.getClip();
+                    clip.open(audioInput);
+                    clip.loop(Clip.LOOP_CONTINUOUSLY); // Enable continuous looping
+                    Thread.sleep(Long.MAX_VALUE); // Keep thread alive to play music
+                } else {
+                    System.out.println("Cannot find file: " + location);
+                }
+            } catch (Exception e) {
+                System.out.println("Error playing music: " + e.getMessage());
+            }
+        }).start();
+    }
+
+    public static void PlayOnce(String location) {
+        new Thread(() -> {
+            try {
+                File musicPath = new File(location);
+                if (musicPath.exists()) {
+                    AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                    clip = AudioSystem.getClip();
+                    clip.open(audioInput);
+                    Thread.sleep(Long.MAX_VALUE); // Keep thread alive to play music
+                } else {
+                    System.out.println("Cannot find file: " + location);
+                }
+            } catch (Exception e) {
+                System.out.println("Error playing music: " + e.getMessage());
+            }
+        }).start();
+    }
+
+    public static void stopMusic() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+            clip.close();
+        }
+    }
+
+    public static void startMusic(){
+        if (clip == null && !clip.isRunning()){
+            String filepath = "MusicFiles/backgroundmusic1.wav";
+            PlayMusic(filepath);
+        }
+    }
+}
